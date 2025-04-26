79ab531f2e3ea0b583bd033118ab8d43c19b9a6e
DAVIDMWELU <davidmwelu@gmail.com>
2025-04-26 03:44:55 +0300

    translating from orm

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,35 @@
-from models import Dog
+# lib/dog.py
 
-def create_table(base):
-    pass
+def create_table(Base, engine):
+    Base.metadata.create_all(engine)
+    engine.dispose() 
 
 def save(session, dog):
-    pass
+    session.add(dog)
+    session.commit()
+    session.close()  # <-- Close here!
 
 def get_all(session):
-    pass
+    dogs = session.query(Dog).all()
+    session.close()  # <-- Close here!
+    return dogs
 
 def find_by_name(session, name):
-    pass
+    dog = session.query(Dog).filter_by(name=name).first()
+    session.close()  # <-- Close here!
+    return dog
 
 def find_by_id(session, id):
-    pass
+    dog = session.query(Dog).filter_by(id=id).first()
+    session.close()  # <-- Close here!
+    return dog
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    dog = session.query(Dog).filter_by(name=name, breed=breed).first()
+    session.close()  # <-- Close here!
+    return dog
 
 def update_breed(session, dog, breed):
-    pass
+    dog.breed = breed
+    session.commit()
+    session.close()  # <-- Close here!
